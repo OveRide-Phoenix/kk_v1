160898c3ee85c69e17c34ab803284f5ea44799f6
OveRide <shashanksrao@gmail.com>
2025-02-20 23:05:55 +0530

    trial of protection #2

--- src/app/bad-code.js
+++ src/app/bad-code.js
@@ -2,5 +2,6 @@ function test(){
     console.log("This is bad practice");
     console.log("Please detect this");
     console.log("Please detect this");
+    console.log("Please detect this!!!!!");
   }
   
